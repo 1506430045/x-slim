b313325ed800a1e819c895336a1bba7eac84c086
xiangqian.xu <xiangqian.xu@thinkbit.com>
2018-07-24 16:43:11 +0800

    deployer

---
+++ config/db/MysqlConfig.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Mysql配置
+ *
+ * User: forward
+ * Date: 2018/6/25
+ * Time: 下午8:50
+ */
+
+namespace Config\db;
+
+
+class MysqlConfig
+{
+    public static $baseConfig = [
+        'host' => '127.0.0.1',
+        'dbname' => 'candy',
+        'username' => 'root',
+        'password' => 'ws_Tb_sas31121F',   //123456 ws_Tb_sas31121F
+        'charset' => 'utf8',
+        'options' => [
+            \PDO::ATTR_STRINGIFY_FETCHES => false,
+            \PDO::ATTR_EMULATE_PREPARES => false,
+            \PDO::MYSQL_ATTR_INIT_COMMAND => "set names utf8",
+            \PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION
+        ]
+    ];
+}
---
+++ config/db/RedisConfig.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Redis配置
+ *
+ * User: forward
+ * Date: 2018/6/25
+ * Time: 下午6:41
+ */
+
+namespace Config\db;
+
+
+class RedisConfig
+{
+    public static $baseConfig = [
+        'host' => '127.0.0.1',
+        'port' => '6379',
+        'timeout' => 1,
+        'auth' => '93541234'
+    ];
+}
