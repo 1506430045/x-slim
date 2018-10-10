eb1ed9989e284e6a586f07af1a48571efdf51aa9
xiangqian.xu <xiangqian.xu@thinkbit.com>
2018-10-10 17:33:21 +0800

    c2c api

--- app/controller/script/TestController.php
+++ app/controller/script/TestController.php
@@ -17,14 +17,48 @@ class TestController extends BaseController
 {
     public function test()
     {
-        try {
-            $list = PdoModel::getInstance(MysqlConfig::$baseConfig)->table('candy_user')->getList(['id', 'nickname', 'phone']);
-            foreach ($list as &$v) {
-                $v['phone_1'] = empty($v['phone']) ? '' : AesUtil::decrypt($v['phone']);
+//        try {
+//            $list = PdoModel::getInstance(MysqlConfig::$baseConfig)->table('candy_user')->getList(['id', 'nickname', 'phone']);
+//            foreach ($list as &$v) {
+//                $v['phone_1'] = empty($v['phone']) ? '' : AesUtil::decrypt($v['phone']);
+//            }
+//            var_dump($list);
+//        } catch (\Exception $e) {
+//            var_dump([]);
+//        }
+        $id = 0;
+        while (true) {
+            $list = $this->getUserListById($id);
+            if (empty($list)) {
+                exit();
             }
-            var_dump($list);
+            $id = $list[count($list) - 1]['id'];
+            $userIds = array_column($list, 'id');
+            $assetList = $this->getAssetByUserIds($userIds);
+            foreach ($list as $v) {
+                echo sprintf("%s,%s,%s,%s", $v['id'], $v['nickname'], AesUtil::decrypt($v['phone']), $assetList[$v['id']]) . PHP_EOL;
+            }
+        }
+    }
+
+    public function getUserListById(int $id = 0)
+    {
+        $userDb = PdoModel::getInstance(MysqlConfig::$baseConfig)->table('candy_user');
+        try {
+            return $userDb->where('id', '>', $id)->limit(200)->getList(['id', 'nickname', 'phone']);
         } catch (\Exception $e) {
-            var_dump([]);
+            return [];
+        }
+    }
+
+    public function getAssetByUserIds(array $userIds)
+    {
+        $assetDb = PdoModel::getInstance(MysqlConfig::$baseConfig)->table('candy_asset');
+        try {
+            $list = $assetDb->whereIn('user_id', $userIds)->getList(['user_id', 'currency_number']);
+            return array_column($list, 'currency_number', 'user_id');
+        } catch (\Exception $e) {
+            return [];
         }
     }
 }
